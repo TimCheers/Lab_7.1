9cd477af01484668f92aecdef142ac930009ff6f
TimCheers <73362594+TimCheers@users.noreply.github.com>
2020-12-08 12:01:28 +0500

    Добавьте файлы проекта.

---
+++ Lab_7/Lab_7.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include<Windows.h>
+#include<ctime>
+using namespace std;
+
+//Написать перегруженные функции и основную программу,
+//которая их вызывает.
+//
+//22
+//а) для массива целых чисел находит количество нечетных
+//элементов;
+//б) для строки находит количество слов в ней.
+
+
+int main()
+{
+    srand(time(NULL));
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int size=-1;
+    string type = "слово";
+    while (size<=0)
+    {
+        cout << "Введите размер массива";
+        cin >> size;
+    }
+    int* arr = new int [size];
+    while (type=="слово")
+    {
+        cout << "Введите способ заполнения массива(случайно, вручную)";
+        cin >> size;
+    }
+    if (type=="случайно")
+    {
+
+    }
+    
+}
+
+
