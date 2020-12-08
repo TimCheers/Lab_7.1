10f27454191b7494342e5e1df8f83e0259fa766b
TimCheers <73362594+TimCheers@users.noreply.github.com>
2020-12-08 19:15:22 +0500

    работа над строками

--- Lab_7/Lab_7.cpp
+++ Lab_7/Lab_7.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include<Windows.h>
 #include<ctime>
+#include<string>
 using namespace std;
 
 //Написать перегруженные функции и основную программу,
@@ -21,20 +22,37 @@ int main()
     string type = "слово";
     while (size<=0)
     {
-        cout << "Введите размер массива";
+        cout << "Введите размер массива чисел";
         cin >> size;
     }
     int* arr = new int [size];
-    while (type=="слово")
+    while (type!="случайно"&&type!="вручную")
     {
         cout << "Введите способ заполнения массива(случайно, вручную)";
-        cin >> size;
+        cin >> type;
     }
     if (type=="случайно")
     {
-
+        for (int i = 0; i < size; i++)
+        {
+            arr[i] = rand() % 20;
+        }
     }
-    
+    else
+    {
+        cout << "Введите массив:" << endl;
+        for (int i = 0; i < size; i++)
+        {
+            cin >> arr[i];
+        }
+    }
+    type = "слово";
+    int sizeS=0;
+    cout << "Введите предложение:";
+    string sentenc;
+    cin >> sentenc;
+    sizeS = sentenc.length();
+    cout << sizeS;
 }
 
 
